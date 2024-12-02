d7d64c95a90eaae04f9350568532f312dbcb8d08
Leonid Vasilev <vasilev.l@panoramikltd.com>
2024-12-02 16:18:52 +0300

    button jump script

--- src/TelegramMiniApp/Assets/Clicker.cs
+++ src/TelegramMiniApp/Assets/Clicker.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Clicker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Button Button;
+    public RectTransform ButtonRectTransform;
+    public RectTransform WindowRectTransform;
+
+    private void Start()
     {
-        
+        Button.onClick.AddListener(OnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClick()
     {
+        float buttonWidth = ButtonRectTransform.rect.width;
+        float buttonHeight = ButtonRectTransform.rect.height;
+        float windowLeft = WindowRectTransform.rect.xMin;
+        float windowRight = WindowRectTransform.rect.xMax;
+        float windowTop = WindowRectTransform.rect.yMax;
+        float windowBottom = WindowRectTransform.rect.yMin;
         
+        float randomX = Random.Range(windowLeft + buttonWidth / 2, windowRight - buttonWidth / 2);
+        float randomY = Random.Range(windowBottom + buttonHeight / 2, windowTop - buttonHeight / 2);
+        
+        ButtonRectTransform.anchoredPosition = new Vector2(randomX, randomY);
     }
 }
